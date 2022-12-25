90ab42cdb55dccdc501ee5b294e8fc1f9e29e62c
zbogus <zahitbogus@gmail.com>
2022-12-25 23:53:16 +0300

    test1

---
+++ contracts/Token.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract BEEToken is ERC20 {
+    constructor() ERC20("BEEToken","BEE"){
+        _mint(msg.sender, 1773000 * 10 ** decimals());
+    }
+}
